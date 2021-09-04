719d5005d84c0fff8733f139d60d74a451626a6e
HuryCB <huryelc.b@gmail.com>
2021-09-04 12:03:59 -0300

    More_Features
    movement, collision, etc

--- RogueLike/Assets/Player.cs
+++ RogueLike/Assets/Player.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private BoxCollider2D boxCollider;
+    private Vector3 moveDelta;
+    private RaycastHit2D hit;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +16,35 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+       
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        moveDelta = new Vector3(x,y,0);
+
+        //swap sprite direction, wether you're going right or left
+        if(moveDelta.x > 0)
+        {
+            transform.localScale = Vector3.one;
+        }
+        else if(moveDelta.x < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        //Make sure we can move in this direciton, by casting a box there first
+        //check with what is going to collide with
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(0,moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Npc", "Blocking"));
+        //check if can move
+        if(hit.collider == null)
+        {
+            transform.Translate(0, moveDelta.y * Time.deltaTime, 0);
+        }
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(moveDelta.x, 0), Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Npc", "Blocking"));
+        //check if can move
+        if(hit.collider == null)
+        {
+            transform.Translate(moveDelta.x * Time.deltaTime, 0, 0);
+        }
         
     }
 }
